103ade1bf3e0f09ea81215dd139f5128ca9ea74b
Nick Hodges <Nick Hodges@localhost>
2013-08-16 14:14:26 -0400

    Set up DateUtils stuff

--- DUT.DateUtils.pas
+++ DUT.DateUtils.pas
@@ -2,6 +2,58 @@ unit DUT.DateUtils;
 
 interface
 
+uses
+       DUnitX.TestFramework
+     , DateUtils
+     , SysUtils
+     ;
+
+type
+  [TestFixture]
+  TDateUtilsOfTests = class
+  var
+    Input: TDateTime;
+    [Setup]
+    procedure Setup;
+  public
+
+    [Test]
+    procedure TestDateOf;
+    procedure TestTimeOf;
+  end;
+
 implementation
+
+{ TDateUtilsOfTests }
+
+procedure TDateUtilsOfTests.Setup;
+begin
+  Input := EncodeDateTime(1944, 1, 2, 3, 4, 5, 6);
+end;
+
+procedure TDateUtilsOfTests.TestDateOf;
+var
+  Expected: TDateTime;
+  Actual: TDateTime;
+begin
+  Expected := EncodeDate(1944, 1, 2);
+  Actual := DateOf(Input);
+  Assert.AreEqual(Expected, Actual);
+end;
+
+procedure TDateUtilsOfTests.TestTimeOf;
+var
+  Expected: TDateTime;
+  Actual: TDateTime;
+begin
+  Expected := EncodeTime(3, 4, 5, 6);
+  Actual := TimeOf(Input);
+  Assert.AreEqual(Expected, Actual);
+
+end;
+
+
+initialization
+  TDUnitX.RegisterTestFixture(TDateUtilsOfTests);
 
 end.
