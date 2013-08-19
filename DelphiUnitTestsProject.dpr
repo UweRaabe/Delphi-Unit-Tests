49f71a0a441a3a146df7d2eb59660726a2b14e6f
Vincent Parrett <vincent@finalbuilder.com>
2013-08-19 17:39:32 +1000

    Renamed some units to use namespaces (with updated DUnitX), modified dpr to enable running on CI server.

--- DelphiUnitTestsProject.dpr
+++ DelphiUnitTestsProject.dpr
@@ -6,23 +6,28 @@ program DelphiUnitTestsProject;
 
 uses
   {$if CompilerVersion < 23 }
-    SysUtils,
+  SysUtils,
   {$else}
-    System.SysUtils, // Delphi XE2 (CompilerVersion 23) added scopes in front of unit names
-  {$ifend}
+  System.SysUtils,
+  {$ifend }
   DUnitX.TestFramework,
   DUnitX.Loggers.Console,
+  DUnitX.Loggers.XML.NUnit,
   DUnitX.Windows.Console,
   DUT.SysUtils in 'DUT.SysUtils.pas',
   DUT.StrUtils in 'DUT.StrUtils.pas',
-  DUT.Classes in 'DUT.Classes.pas',
-  DUT.DateUtils in 'DUT.DateUtils.pas';
+  DUT.Classes.TList.Enumerator in 'DUT.Classes.TList.Enumerator.pas',
+  DUT.DateUtils.Encode in 'DUT.DateUtils.Encode.pas',
+  DUT.DateUtils.DateOf in 'DUT.DateUtils.DateOf.pas';
 
 var
   Runner: ITestRunner;
   Logger: ITestLogger;
   Results: ITestResults;
-
+  {$IFDEF CI}
+  XMLLogger : ITestLogger;
+  xmlFilePath : string;
+  {$ENDIF}
 begin
   try
     //Create the runner
@@ -32,11 +37,24 @@ begin
     Logger := TDUnitXConsoleLogger.Create;
     runner.AddLogger(Logger);
 
+    {$IFDEF CI}
+    //When running under the CI Server we want the results as Nunit xml
+      xmlFilePath := ParamStr(1);
+      ForceDirectories(ExcludeTrailingPathDelimiter(ExtractFilePath(xmlFilePath)));
+      XMLLogger := TDUnitXXMLNUnitFileLogger.Create(xmlFilePath);
+      runner.AddLogger(XMLLogger);
+
+    {$ENDIF}
+
+
     //Run tests
     Results := Runner.Execute;
 
+    {$IFNDEF CI}
+    //We don't want this when running on a CI server.
     System.Write('Done.. press the <Enter> key to quit.');
     System.Readln;
+    {$ENDIF}
   except
     on E: Exception do
       System.Writeln(E.ClassName, ': ', E.Message);
